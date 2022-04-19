271cac05cae4cf8e84b5a0117ed39d64b27c9c2b
Hichami Houssam <hichami.houssam@gmail.com>
2022-04-19 20:17:48 +0000

    challenge2 solution

---
+++ spiralMatrix/challenge.js
@@ -0,0 +1,19 @@
+function spiralCopy(inputMatrix) {
+    let arr = []
+    let matrix = inputMatrix
+    if(matrix.length == 1){
+      return [].concat(...matrix)
+    }
+    while(matrix.length){
+      arr.push(matrix.shift(), matrix.map(ar => ar.pop()), (matrix.pop()).reverse(), matrix.map(ar => ar.shift()).reverse())
+    }
+   return [].concat(...arr)
+  }
+  
+  arr = spiralCopy([ [1,    2,   3,  4,    5],
+                           [6,    7,   8,  9,   10],
+                           [11,  12,  13,  14,  15],
+                           [16,  17,  18,  19,  20] ])
+  
+  console.log(arr)
+
